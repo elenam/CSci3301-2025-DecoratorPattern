9cdebdb76965c958ed3339b866a421ac60f04109
Elena M <elenam@morris.umn.edu>
2025-02-05 12:24:44 -0600

    added the question on adding beverage sizes

--- src/starbuzzSizes/Beverage.java
+++ src/starbuzzSizes/Beverage.java
@@ -1,6 +1,10 @@
 package starbuzzSizes;
 
 public abstract class Beverage {
+	// There are different sizes of a beverage:
+	public enum Size { TALL, GRANDE, VENTI };
+	// The default is TALL:
+	protected Size size = Size.TALL;
 	protected String description = "Unknown Beverage";
   
 	public String getDescription() {
--- src/starbuzzSizes/StarbuzzCoffee.java
+++ src/starbuzzSizes/StarbuzzCoffee.java
@@ -21,4 +21,28 @@ public class StarbuzzCoffee {
 		System.out.println(beverage3.getDescription() 
 				+ " $" + beverage3.cost());
 	}
+	
+	/* 
+	 * We would like to be able to create beverages of different sizes
+	 * The cost of the beverage for be adjusted by size:
+	 * 
+	 * Size.TALL adjustment is 1.0 (it's the standard size)
+	 * Size.GRANDE adjustment is 1.5 
+	 * Size.VENTE adjustment is 2.0 
+	 * 
+	 * The adjustment must be applied to the coffee (the basis of beverages)
+	 * The cost of different condiments may or may not be adjusted. 
+	 * In our example the amount of whipped cream and mocha is the same 
+	 * for all sizes of beverages, but the amount of milk is adjusted with 
+	 * the size. 
+	 * 
+	 * Note the added `size` variable in the Beverage class. 
+	 * The book proposes adding setSize() and getSize() methods to the Beverage class. 
+	 * Discuss the benefits and downsides of adding each of these two methods; think
+	 * of their possible implementation.  
+	 * 
+	 * Think carefully about what objects we are creating and where they get their
+	 * instance variable. Propose an approach that can't create incorrect or 
+	 * misleading results. 
+	 */
 }
